e52c3099c87511dcc520fd0ecb91f3cbfcabec8e
4klb <khlbolatovna@gmail.com>
2023-03-21 00:29:18 +0600

    implements rabbit connection

--- dish/broker/broker.go
+++ dish/broker/broker.go
@@ -1,6 +1,24 @@
 package broker
 
-func GetRabbitConnection() {}
+import (
+	"fmt"
+	"log"
+
+	"github.com/4klb/coffeetime/dish/config"
+	"github.com/streadway/amqp"
+)
+
+func GetRabbitConnection() (*amqp.Connection, error) {
+	uri := fmt.Sprintf("amqp://%v:%v@%v:%v/", config.GetConfig().RabbitMQ.User, config.GetConfig().RabbitMQ.Password, config.GetConfig().RabbitMQ.Host, config.GetConfig().RabbitMQ.Port)
+
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	return conn, nil
+}
 
 func GetExchange() {}
 
