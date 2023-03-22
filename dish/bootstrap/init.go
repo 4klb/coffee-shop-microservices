0cabce9b48c7b88a51060641b69af450d9a1c7c4
4klb <khlbolatovna@gmail.com>
2023-03-23 02:57:51 +0600

    some changes

--- dish/bootstrap/init.go
+++ dish/bootstrap/init.go
@@ -1,9 +1,15 @@
 package bootstrap
 
 import (
+	"log"
+
 	"github.com/4klb/coffee-shop-microservices/dish/broker"
 )
 
-func InitRabbit() {
-	broker.GetRabbitConnection()
+func InitRabbit() *broker.RabbitMQ {
+	rmq, err := broker.GetRabbitConnection()
+	if err != nil {
+		log.Panic(err)
+	}
+	return rmq
 }
