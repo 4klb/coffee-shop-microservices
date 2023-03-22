949b19bfe0272477b856cb53868e1ba3d2fa98ba
4klb <khlbolatovna@gmail.com>
2023-03-23 02:58:46 +0600

    adds rabbit conn, exchange

--- dish/broker/broker.go
+++ dish/broker/broker.go
@@ -2,24 +2,60 @@ package broker
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/4klb/coffee-shop-microservices/dish/config"
 	"github.com/streadway/amqp"
 )
 
-func GetRabbitConnection() (*amqp.Connection, error) {
-	uri := fmt.Sprintf("amqp://%v:%v@%v:%v/", config.GetConfig().RabbitMQ.User, config.GetConfig().RabbitMQ.Password, config.GetConfig().RabbitMQ.Host, config.GetConfig().RabbitMQ.Port)
+type RabbitMQ struct {
+	Connection *amqp.Connection
+	Channel    *amqp.Channel
+}
+
+func GetRabbitConnection() (*RabbitMQ, error) {
+	uri := fmt.Sprintf("amqp://%v:%v@%v:%v/",
+		config.GetConfig().RabbitMQ.User,
+		config.GetConfig().RabbitMQ.Password,
+		config.GetConfig().RabbitMQ.Host,
+		config.GetConfig().RabbitMQ.Port)
 
 	conn, err := amqp.Dial(uri)
 	if err != nil {
-		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+		return nil, fmt.Errorf("amqp.Dial %w", err)
 	}
 	fmt.Println("Successfully connected to RabbitMQ")
 
-	return conn, nil
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("conn.Channel %w", err)
+	}
+
+	if err = ch.ExchangeDeclare(
+		config.GetConfig().RabbitMQ.ExchangeName,
+		config.GetConfig().RabbitMQ.ExchangeType,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return nil, fmt.Errorf("ch.ExchangeDeclare %w", err)
+	}
+
+	// if err = ch.Qos(
+	// 	0,
+	// 	1,
+	// 	false,
+	// ); err != nil {
+	// 	return nil, fmt.Errorf("ch.Qos %w", err)
+	// }
+
+	return &RabbitMQ{
+		Connection: conn,
+		Channel:    ch,
+	}, err
 }
 
-func GetExchange() {}
-
-func PutDishIntoQueue() {}
+func (r *RabbitMQ) Close() {
+	r.Connection.Close()
+}
