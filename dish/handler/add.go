3a2c73612b6ae010c081cba58278d7dcdbc62e11
4klb <khlbolatovna@gmail.com>
2023-03-23 03:02:08 +0600

    msg into rmq

--- dish/handler/add.go
+++ dish/handler/add.go
@@ -2,16 +2,20 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 
 	uuid "github.com/satori/go.uuid"
 
 	pb "github.com/4klb/coffee-shop-microservices/proto/dish"
 
+	"github.com/4klb/coffee-shop-microservices/dish/broker"
+	"github.com/4klb/coffee-shop-microservices/dish/config"
 	"github.com/4klb/coffee-shop-microservices/dish/storage/repo"
 )
 
 func Add(ctx context.Context, req *pb.AddDishReq) error {
-	//todo validate
+	//todo req check
 
 	dishInfo := repo.Dish{
 		Id:          uuid.Must(uuid.NewV4(), nil).String(),
@@ -20,9 +24,25 @@ func Add(ctx context.Context, req *pb.AddDishReq) error {
 		Price:       req.Dish.Price,
 	}
 
-	if err := dishInfo.InsertDish(ctx); err != nil {
-		return err
+	// if err := dishInfo.InsertDish(ctx); err != nil {
+	// 	return err
+	// }
+
+	rmq, err := broker.GetRabbitConnection()
+	if err != nil {
+		log.Fatalf("broker.GetRabbitConnection: %s", err)
 	}
+
+	publisher, err := broker.NewPublisher(rmq)
+	if err != nil {
+		log.Fatalf("broker.NewPublisher: %s", err)
+	}
+
+	ctxb := context.Background()
+
+	msg, err := json.Marshal(dishInfo)
+
+	publisher.Created(ctxb, config.GetConfig().RabbitMQ.RoutingKey, msg)
 
 	return nil
 }
