167f7fe9121ad43c1c800dc9ac638651c38cd318
4klb <khlbolatovna@gmail.com>
2023-03-26 05:56:04 +0600

    moves run func

--- dish/server/server.go
+++ dish/server/server.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net"
 
+	"github.com/4klb/coffee-shop-microservices/dish/broker"
 	"github.com/4klb/coffee-shop-microservices/dish/config"
 	pb "github.com/4klb/coffee-shop-microservices/proto/dish"
 	"google.golang.org/grpc"
@@ -13,7 +15,37 @@ type ServerDish struct {
 	pb.UnimplementedDishServiceServer
 }
 
+func Run() {
+	var consumer broker.Consumer
+
+	go func() {
+		if err := InitDishServer(); err != nil {
+			log.Fatalf("Couldn't run dish server: %s", err)
+		}
+	}()
+
+	rmq, err := broker.GetRabbitConnection()
+	if err != nil {
+		log.Fatalf("broker.GetRabbitConnection: %s", err)
+	}
+
+	errC := make(<-chan error, 1)
+
+	go func() {
+		errC, err = consumer.RunConsumer(rmq)
+		if err != nil {
+			log.Fatalf("Couldn't run: %s", err)
+		}
+
+	}()
+	if err := <-errC; err != nil {
+		log.Fatalf("Error while running: %s", err)
+	}
+}
+
 func InitDishServer() error {
+	log.Printf("Dish server is running...")
+
 	listener, err := net.Listen(
 		config.GetConfig().DishServer.Network,
 		config.GetConfig().DishServer.Host+config.GetConfig().DishServer.Port)
