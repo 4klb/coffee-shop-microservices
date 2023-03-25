b06ebd174e703896cc333956c10cd20105dd8d52
4klb <khlbolatovna@gmail.com>
2023-03-26 05:54:59 +0600

    invokes run

--- dish/main.go
+++ dish/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/4klb/coffee-shop-microservices/dish/server"
 	"github.com/joho/godotenv"
 )
@@ -10,16 +8,5 @@ import (
 func main() {
 	godotenv.Load()
 
-	if err := server.InitDishServer(); err != nil {
-		log.Fatalf("Couldn't run dish server: %s", err)
-	}
-
-	// errC, err := server.Run(rmq)
-	// if err != nil {
-	// 	log.Fatalf("Couldn't run: %s", err)
-	// }
-
-	// if err := <-errC; err != nil {
-	// 	log.Fatalf("Error while running: %s", err)
-	// }
+	server.Run()
 }
