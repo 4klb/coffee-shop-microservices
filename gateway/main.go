dd5c909e02ee6a0a55022cf94d3c2382161c0512
4klb <khlbolatovna@gmail.com>
2023-03-27 00:25:40 +0600

    adds run

--- gateway/main.go
+++ gateway/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/4klb/coffee-shop-microservices/gateway/internal/server"
+	"github.com/joho/godotenv"
+)
 
+func main() {
+	godotenv.Load()
+
+	server.Run()
 }
