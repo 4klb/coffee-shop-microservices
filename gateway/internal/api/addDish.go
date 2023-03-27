2e567e5224290af6d821f9010e4e088f876b7872
4klb <khlbolatovna@gmail.com>
2023-03-27 14:46:47 +0600

    adds handleAddDish

--- gateway/internal/api/addDish.go
+++ gateway/internal/api/addDish.go
@@ -1,21 +1,53 @@
 package api
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/4klb/coffee-shop-microservices/gateway/internal/models"
 	"github.com/4klb/coffee-shop-microservices/gateway/internal/response"
 	"github.com/gin-gonic/gin"
+
+	"github.com/4klb/coffee-shop-microservices/gateway/internal/service/dish"
+	pb "github.com/4klb/coffee-shop-microservices/proto/dish"
 )
 
-//AddDish ..
-func (h *Handle) AddDish(c *gin.Context) { //TODO
-	var dish models.Dish
+const defaultCtx = time.Second * 10
 
-	if err := c.BindJSON(&dish); err != nil {
+//AddDish ..
+func (h *Handle) AddDish(c *gin.Context) {
+	var dishreq models.Dish
+
+	if err := c.BindJSON(&dishreq); err != nil {
+		response.ResponseWithError(http.StatusBadRequest, err, c)
+		return
+	}
+
+	if err := AddDish(dishreq); err != nil {
 		response.ResponseWithError(http.StatusBadRequest, err, c)
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Data was added"})
+}
+
+func AddDish(dishreq models.Dish) error {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultCtx)
+	defer cancel()
+
+	dishProto := &pb.Dish{
+		Id:          dishreq.Id,
+		Name:        dishreq.Name,
+		Description: dishreq.Description,
+		Price:       dishreq.Price,
+	}
+
+	if err := dish.Add(ctx, &pb.AddDishReq{
+		Dish: dishProto,
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
